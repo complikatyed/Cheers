a3dd993b0813dce8bef3725d281fc28e55cb3511
Katye Russell <katye.russell@gmail.com>
2015-05-01 14:46:16 -0500

    Corrected to simplified version (which works).

--- cheers.rb
+++ cheers.rb
@@ -1,19 +1,11 @@
-# This is my test version of the second or third iteration that we worked on in class today.
-# It doesn't really work, but I wanted to try it out to see what would happen.
+puts "Hello, What's your name?"
+name = gets.chomp.upcase
 
-def ask_for_name
-  puts "Hello, What's your name?"
-  gets.chomp.upcase
-end
-
-
-while ask_for_name.empty?
-  ask_for_name
-end
 an_letters = "AEFHILMNORSX"
-ask_for_name.each_char do |letter|
+name.each_char do |letter|
   article = an_letters.include?(letter) ? "an" : "a"
   puts "Give me #{article}... #{letter}!"
 end
 
-puts ask_for_name + "'s just GRAND!"
+puts "#{name}'s just GRAND!"
+
